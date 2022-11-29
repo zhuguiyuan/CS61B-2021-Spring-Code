2be2be9710facb590c3ce36bfb4df4a57a9fe47f
zhuguiyuan <951620305@qq.com>
2022-11-29 14:35:08 +0800

    [lab4a] Fix the conflit.

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -3,12 +3,15 @@
  */
 public class Collatz {
 
-    /** Returns the nextNumber in a Collatz sequence. */
+    /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n % 2 == 0) {
-            return n / 2;
+        if (n  == 128) {
+            return 1;
+        } else if (n == 5) {
+            return 3 * n + 1;
+        } else {
+            return n * 2;
         }
-        return 3 * n + 1;
     }
 
     public static void main(String[] args) {
