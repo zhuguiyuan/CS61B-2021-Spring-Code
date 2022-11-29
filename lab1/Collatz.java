b18355df335eab5b16b40a4fb8cb43a62dbfdace
zhuguiyuan <951620305@qq.com>
2022-11-29 14:37:16 +0800

    [lab4b] Restore lab1 file.

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -3,15 +3,12 @@
  */
 public class Collatz {
 
-    /** Buggy implementation of nextNumber! */
+    /** Returns the nextNumber in a Collatz sequence. */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
-        } else {
-            return n * 2;
+        if (n % 2 == 0) {
+            return n / 2;
         }
+        return 3 * n + 1;
     }
 
     public static void main(String[] args) {
