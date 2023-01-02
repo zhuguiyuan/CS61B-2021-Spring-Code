b3ef4d3bdfc37ae41f92ab94bab5d2d7ddaf84b1
zhuguiyuan <951620305@qq.com>
2023-01-02 14:01:20 +0800

    [lab7] Complete lab7.

--- lab7/bstmap/BSTMap.java
+++ lab7/bstmap/BSTMap.java
@@ -1,7 +1,10 @@
 package bstmap;
 
+import edu.princeton.cs.algs4.Stack;
+
 import java.util.Iterator;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
     private BSTNode<K, V> root;
@@ -23,17 +26,17 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
         }
     }
 
-    private BSTNode insert(BSTNode<K, V> root, K key, V value) {
+    private BSTNode<K, V> insert(BSTNode<K, V> root, K key, V value) {
         if (root == null) {
-            return new BSTNode(key, value);
+            return new BSTNode<>(key, value);
         }
         int compare = key.compareTo(root.key);
-        if (compare == 0) {
-            root.value = value;
-        } else if (compare < 0) {
+        if (compare < 0) {
             root.left = insert(root.left, key, value);
-        } else {
+        } else if (compare > 0) {
             root.right = insert(root.right, key, value);
+        } else {
+            root.value = value;
         }
         root.size = size(root.left) + size(root.right) + 1;
         return root;
@@ -70,12 +73,12 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
             return null;
         }
         int compare = key.compareTo(root.key);
-        if (compare == 0) {
-            return root;
-        } else if (compare < 0) {
+        if (compare < 0) {
             return search(root.left, key);
-        } else {
+        } else if (compare > 0) {
             return search(root.right, key);
+        } else {
+            return root;
         }
     }
 
@@ -94,22 +97,103 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
 
     @Override
     public Set<K> keySet() {
-        throw new UnsupportedOperationException();
+        Set<K> result = new TreeSet<>();
+        for (K key : this) {
+            result.add(key);
+        }
+        return result;
     }
 
     @Override
     public V remove(K key) {
-        throw new UnsupportedOperationException();
+        V value = get(key);
+        root = delete(root, key);
+        return value;
     }
 
     @Override
     public V remove(K key, V value) {
-        throw new UnsupportedOperationException();
+        if (value.equals(get(key))) {
+            root = delete(root, key);
+        }
+        return value;
+    }
+
+    private BSTNode<K, V> delete(BSTNode<K, V> root, K key) {
+        if (root == null) {
+            return null;
+        }
+        int compare = key.compareTo(root.key);
+        if (compare < 0) {
+            root.left = delete(root.left, key);
+        } else if (compare > 0) {
+            root.right = delete(root.right, key);
+        } else {
+            if (root.right == null) {
+                return root.left;
+            }
+            if (root.left == null) {
+                return root.right;
+            }
+            BSTNode<K, V> tmp = root;
+            root = min(tmp.right);
+            root.right = deleteMin(tmp.right);
+            root.left = tmp.left;
+        }
+        root.size = size(root.left) + size(root.right) + 1;
+        return root;
+    }
+
+    private BSTNode<K, V> min(BSTNode<K, V> root) {
+        assert root != null;
+        while (root.left != null) {
+            root = root.left;
+        }
+        return root;
+    }
+
+    private BSTNode<K, V> deleteMin(BSTNode<K, V> root) {
+        if (root.left == null) {
+            return root.right;
+        }
+        root.left = deleteMin(root.left);
+        root.size = size(root.left) + size(root.right) + 1;
+        return root;
     }
 
     @Override
     public Iterator<K> iterator() {
-        throw new UnsupportedOperationException();
+        return new BSTMapIterator();
+    }
+
+    private class BSTMapIterator implements Iterator<K> {
+        private final Stack<BSTNode<K, V>> stack;
+
+        BSTMapIterator() {
+            stack = new Stack<>();
+            pushLeftToLeaf(root);
+        }
+
+        private void pushLeftToLeaf(BSTNode<K, V> root) {
+            if (root != null) {
+                stack.push(root);
+                pushLeftToLeaf(root.left);
+            }
+        }
+
+        @Override
+        public boolean hasNext() {
+            return !stack.isEmpty();
+        }
+
+        @Override
+        public K next() {
+            BSTNode<K, V> curr = stack.pop();
+            if (curr.right != null) {
+                pushLeftToLeaf(curr.right);
+            }
+            return curr.key;
+        }
     }
 
     /**
@@ -117,6 +201,9 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
      * Used for Debugging.
      */
     public void printInOrder() {
-
+        for (K key : this) {
+            System.out.printf("%s ", key);
+        }
+        System.out.println();
     }
 }
